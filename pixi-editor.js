59079f0f1e9aae4dac6191f9099f19ef3581d99f
sergey <serge.dmitriev@gmail.com>
2014-08-24 12:59:55 +0800

    change demo layout

--- pixi-editor.js
+++ pixi-editor.js
@@ -1,140 +1,5 @@
 "use strict";
 
-var Canvas = function(id, width, height){
-	
-	var stage = null;
-	var renderer = null;
-
-	var mode = 'draw';
-	var drawing = false;
-	var path = [];
-
-	var graphics = new PIXI.Graphics();
-	var liveGraphics = new PIXI.Graphics();
-
-	var lineWidth = '10';
-	var lineColor = '0xffd920';
-	var lineDrawingColor = '0xffd920';
-	var lineAlpha = '1';
-	
-	var enableDrawMode = function(){
-		
-		setDrawEvents();
-	}
-
-	var disableDrawMode = function(){
-		
-		dropDrawEvents();
-	}
-
-	var setDrawEvents = function(){
-		stage.mousedown = stage.touchstart = function(e) {
-			path = [];
-			var position = e.global;
-		    drawing = true;
-		};
-
-		stage.mousemove = stage.touchmove = function(e) {
-			if(!drawing){return}
-			var position = e.global;
-			path.push(parseInt(position.x));
-			path.push(parseInt(position.y));
-		};
-
-		stage.mouseup = stage.touchend = function(e){
-			drawing = false;
-			graphics.lineStyle(lineWidth, lineColor, lineAlpha);
-			graphics.drawPath(path);
-			path = [];
-		}
-	}
-
-	var dropDrawEvents = function(){
-		stage.mousedown = stage.touchstart = '';
-		stage.mousemove = stage.touchmove = '';
-		stage.mouseup = stage.touchend = '';
-	}
-
-	var animate = function() {
-
-	    liveGraphics.clear();
-		liveGraphics.lineStyle(lineWidth, lineDrawingColor, lineAlpha);
-		liveGraphics.drawPath(path);
-	    
-	    renderer.render(stage);
-	    requestAnimFrame(animate);
-	}
-
-	var setLineWidth = function(width){
-		lineWidth = width;
-	}
-
-	var getLineWidth = function(){
-		return lineWidth;
-	}
-
-	var setBackgroundColor = function(color){
-		stage.setBackgroundColor(color);
-	}
-
-	var setLineColor = function(color){
-		lineColor = color;
-		lineDrawingColor = color;		
-	}
-
-	var setLineAlpha = function(alpha){
-		lineAlpha = alpha;
-	}
-
-	var getLineAlpha = function(){
-		return lineAlpha;
-	}
-
-	var getImage = function(){
-		renderer.render(stage);
-		return renderer.view.toDataURL();		
-	}
-
-	var clear = function(){
-		graphics.clear();
-	}
-
-	var __construct = function(id, width, height) {
-        stage = new PIXI.Stage(0x97c56e, true);
-        
-        renderer = new PIXI.autoDetectRenderer(width, height, null, false, true);
-        
-        stage.transparent = true;
-        document.getElementById(id).appendChild(renderer.view);
-        renderer.view.style.position = "relative";
-        renderer.render(stage); 
-
-        stage.addChild(graphics);
-		stage.addChild(liveGraphics);
-
-
-		animate();
-
-    }(id, width, height);
-
-	return {
-		enableDrawMode: enableDrawMode,
-		disableDrawMode: disableDrawMode,
-		setLineWidth: setLineWidth,
-		getLineWidth: getLineWidth,
-		setBackgroundColor: setBackgroundColor,
-		setLineColor: setLineColor,
-		setLineAlpha: setLineAlpha,
-		getLineAlpha: getLineAlpha,
-		getImage: getImage,
-		stage: stage,
-		clear: clear,
-	};
-};
-
-
-
-
 var editor = function(id, width, height){
 
 	var onSaveCallback = null;
@@ -144,6 +9,138 @@ var editor = function(id, width, height){
 		lineWidth: 10,
 		lineAlpha: 1,
 		backgroundColor: '#97c56e',
+	};
+
+	var Canvas = function(id, width, height){
+		
+		var stage = null;
+		var renderer = null;
+
+		var mode = 'draw';
+		var drawing = false;
+		var path = [];
+
+		var graphics = new PIXI.Graphics();
+		var liveGraphics = new PIXI.Graphics();
+
+		var lineWidth = '10';
+		var lineColor = '0xffd920';
+		var lineDrawingColor = '0xffd920';
+		var lineAlpha = '1';
+		
+		var enableDrawMode = function(){
+			
+			setDrawEvents();
+		}
+
+		var disableDrawMode = function(){
+			
+			dropDrawEvents();
+		}
+
+		var setDrawEvents = function(){
+			stage.mousedown = stage.touchstart = function(e) {
+				path = [];
+				var position = e.global;
+			    drawing = true;
+			};
+
+			stage.mousemove = stage.touchmove = function(e) {
+				if(!drawing){return}
+				var position = e.global;
+				path.push(parseInt(position.x));
+				path.push(parseInt(position.y));
+			};
+
+			stage.mouseup = stage.touchend = function(e){
+				drawing = false;
+				graphics.lineStyle(lineWidth, lineColor, lineAlpha);
+				graphics.drawPath(path);
+				path = [];
+			}
+		}
+
+		var dropDrawEvents = function(){
+			stage.mousedown = stage.touchstart = '';
+			stage.mousemove = stage.touchmove = '';
+			stage.mouseup = stage.touchend = '';
+		}
+
+		var animate = function() {
+
+		    liveGraphics.clear();
+			liveGraphics.lineStyle(lineWidth, lineDrawingColor, lineAlpha);
+			liveGraphics.drawPath(path);
+		    
+		    renderer.render(stage);
+		    requestAnimFrame(animate);
+		}
+
+		var setLineWidth = function(width){
+			lineWidth = width;
+		}
+
+		var getLineWidth = function(){
+			return lineWidth;
+		}
+
+		var setBackgroundColor = function(color){
+			stage.setBackgroundColor(color);
+		}
+
+		var setLineColor = function(color){
+			lineColor = color;
+			lineDrawingColor = color;		
+		}
+
+		var setLineAlpha = function(alpha){
+			lineAlpha = alpha;
+		}
+
+		var getLineAlpha = function(){
+			return lineAlpha;
+		}
+
+		var getImage = function(){
+			renderer.render(stage);
+			return renderer.view.toDataURL();		
+		}
+
+		var clear = function(){
+			graphics.clear();
+		}
+
+		var __construct = function(id, width, height) {
+	        stage = new PIXI.Stage(0x97c56e, true);
+	        
+	        renderer = new PIXI.autoDetectRenderer(width, height, null, false, true);
+	        
+	        stage.transparent = true;
+	        document.getElementById(id).appendChild(renderer.view);
+	        renderer.view.style.position = "relative";
+	        renderer.render(stage); 
+
+	        stage.addChild(graphics);
+			stage.addChild(liveGraphics);
+
+
+			animate();
+
+	    }(id, width, height);
+
+		return {
+			enableDrawMode: enableDrawMode,
+			disableDrawMode: disableDrawMode,
+			setLineWidth: setLineWidth,
+			getLineWidth: getLineWidth,
+			setBackgroundColor: setBackgroundColor,
+			setLineColor: setLineColor,
+			setLineAlpha: setLineAlpha,
+			getLineAlpha: getLineAlpha,
+			getImage: getImage,
+			stage: stage,
+			clear: clear,
+		};
 	};
 
 	var Panel = function(id){
@@ -199,9 +196,9 @@ var editor = function(id, width, height){
 				min: 1, 
 				max: 20,
 				tooltip: 'hide',
-				value: canvas2.getLineWidth(),
+				value: canvas.getLineWidth(),
 			}).on('slide', function(e){
-				canvas2.setLineWidth(e.value)
+				canvas.setLineWidth(e.value)
 			});
 
 			$('#lineAlphaSlider').slider({
@@ -209,17 +206,17 @@ var editor = function(id, width, height){
 				max: 1.0,
 				step: 0.1,
 				tooltip: 'hide',
-				value: canvas2.getLineAlpha(),
+				value: canvas.getLineAlpha(),
 			}).on('slide', function(e){
-				canvas2.setLineAlpha(e.value)
+				canvas.setLineAlpha(e.value)
 			});
 			
 			$("#saveButton").click(function(){
-				onSaveCallback(canvas2.getImage());
+				onSaveCallback(canvas.getImage());
 			});
 
 			$("#clearButton").click(function(){
-				canvas2.clear();
+				canvas.clear();
 			});
 
 			$("#changeLineColorInput").spectrum({
@@ -230,7 +227,7 @@ var editor = function(id, width, height){
 		        	['green', 'yellow', 'purple']
 		    	],
 		   		change: function(color) {
-		 	   		canvas2.setLineColor(color.toHexString().replace('#','0x'));
+		 	   		canvas.setLineColor(color.toHexString().replace('#','0x'));
 				}
 			});
 
@@ -242,7 +239,7 @@ var editor = function(id, width, height){
 		        	['green', 'yellow', 'purple']
 		    	],
 		   		change: function(color) {
-		 	   		canvas2.setBackgroundColor(color.toHexString().replace('#','0x'));
+		 	   		canvas.setBackgroundColor(color.toHexString().replace('#','0x'));
 				}
 			});
 
@@ -264,13 +261,13 @@ var editor = function(id, width, height){
 	$('#'+id).append($('<div />',{id: id + 'Canvas'}));
 	$('#'+id).append($('<div />',{id: id + 'Bottom'}));
 
-	var canvas2 = new Canvas(id + 'Canvas', canvasSettings.width, canvasSettings.height);
-	canvas2.enableDrawMode();
+	var canvas = new Canvas(id + 'Canvas', canvasSettings.width, canvasSettings.height);
+	canvas.enableDrawMode();
 	
 	var panel = new Panel(id);
 	
 	return {
 		onSave: onSave,
-		getImage: canvas2.getImage,
+		getImage: canvas.getImage,
 	}
 }
